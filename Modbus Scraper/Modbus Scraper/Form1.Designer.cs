f1a9c73cb7b45cc68c5ce9c19dadb60908b04c93
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2022-01-10 22:44:03 +0200

    Add files via upload

---
+++ Modbus Scraper/Modbus Scraper/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Modbus_Scraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.save = new System.Windows.Forms.Button();
+            this.port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipadd = new System.Windows.Forms.TextBox();
+            this.richedit = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(142, 388);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 13;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click_1);
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(194, 45);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(41, 20);
+            this.port.TabIndex = 12;
+            this.port.Text = "502";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "IP Address";
+            // 
+            // ipadd
+            // 
+            this.ipadd.Location = new System.Drawing.Point(44, 45);
+            this.ipadd.Name = "ipadd";
+            this.ipadd.Size = new System.Drawing.Size(123, 20);
+            this.ipadd.TabIndex = 9;
+            this.ipadd.Text = "127.0.0.1";
+            // 
+            // richedit
+            // 
+            this.richedit.Location = new System.Drawing.Point(44, 84);
+            this.richedit.Name = "richedit";
+            this.richedit.Size = new System.Drawing.Size(603, 285);
+            this.richedit.TabIndex = 8;
+            this.richedit.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipadd);
+            this.Controls.Add(this.richedit);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipadd;
+        private System.Windows.Forms.RichTextBox richedit;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Modbus Scraper/Modbus Scraper/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace Modbus_Scraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string fileName = @"C:\Windows\Temp\log.txt";
+
+            if (File.Exists(fileName))
+            {
+                File.AppendAllText(fileName, richedit.Text);
+            }
+            else
+            {
+                try
+                {
+                    // Create a new file     
+                    using (FileStream fs = File.Create(fileName))
+                    {
+                        File.AppendAllText(fileName, richedit.Text);
+                    }
+                }
+                catch
+                { }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int portnum = Int32.Parse(port.Text);
+            ModbusClient modbusClient = new ModbusClient(ipadd.Text, portnum);         //Ip-Address and Port of Modbus-TCP-Server
+            modbusClient.Connect();                                                    //Connect to Server
+
+            int[] readHoldingRegisters = new int[10000];
+            int index1 = 0;
+
+            for (int x = 0; x < 100; x++)
+            {
+                readHoldingRegisters = modbusClient.ReadHoldingRegisters(x * 100, 100);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    richedit.AppendText("\r\n" + +(index1) + "    :    " + readHoldingRegisters[i].ToString());
+                    richedit.ScrollToCaret();
+                    index1++;
+                }
+            }
+            modbusClient.Disconnect();                                                //Disconnect from Server
+        }
+
+        private void save_Click_1(object sender, EventArgs e)
+        {
+            string fileName = @"C:\Windows\Temp\log.txt";
+
+            if (File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, String.Empty);
+                File.AppendAllText(fileName, richedit.Text);
+            }
+            else
+            {
+                try
+                {
+                    // Create a new file     
+                    using (FileStream fs = File.Create(fileName))
+                    {
+                        File.WriteAllText(fileName, String.Empty);
+                        File.AppendAllText(fileName, richedit.Text);
+                    }
+                }
+                catch
+                { }
+            }
+        }
+    }
+}
