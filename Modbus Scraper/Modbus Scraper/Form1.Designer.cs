9d1e45f93d4e44dc833fede53f4aae4d920e9425
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2022-01-11 22:28:38 +0200

    Added Node ID Search

--- Modbus Scraper/Modbus Scraper/Form1.Designer.cs
+++ Modbus Scraper/Modbus Scraper/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.save = new System.Windows.Forms.Button();
             this.port = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,21 +37,35 @@
             this.richedit = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.lblwait = new System.Windows.Forms.Label();
             this.num = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.startreg = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
-            this.overflow = new System.Windows.Forms.RadioButton();
             this.label5 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.slaveid = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.port2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ipaddr2 = new System.Windows.Forms.TextBox();
+            this.id1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.id2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richedit2 = new System.Windows.Forms.RichTextBox();
+            this.lblwait = new System.Windows.Forms.Label();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.brkbox = new System.Windows.Forms.CheckBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.showextrm = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.num)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.startreg)).BeginInit();
             this.SuspendLayout();
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(239, 388);
+            this.save.Location = new System.Drawing.Point(259, 388);
             this.save.Name = "save";
             this.save.Size = new System.Drawing.Size(75, 23);
             this.save.TabIndex = 13;
@@ -60,7 +75,7 @@
             // 
             // port
             // 
-            this.port.Location = new System.Drawing.Point(194, 45);
+            this.port.Location = new System.Drawing.Point(173, 45);
             this.port.Name = "port";
             this.port.Size = new System.Drawing.Size(41, 20);
             this.port.TabIndex = 12;
@@ -69,7 +84,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(191, 29);
+            this.label2.Location = new System.Drawing.Point(170, 29);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 11;
@@ -94,15 +109,15 @@
             // 
             // richedit
             // 
-            this.richedit.Location = new System.Drawing.Point(44, 84);
+            this.richedit.Location = new System.Drawing.Point(44, 135);
             this.richedit.Name = "richedit";
-            this.richedit.Size = new System.Drawing.Size(603, 285);
+            this.richedit.Size = new System.Drawing.Size(290, 234);
             this.richedit.TabIndex = 8;
             this.richedit.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(44, 388);
+            this.button1.Location = new System.Drawing.Point(51, 388);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 7;
@@ -112,7 +127,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(142, 388);
+            this.button2.Location = new System.Drawing.Point(159, 388);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 14;
@@ -120,20 +135,9 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // lblwait
-            // 
-            this.lblwait.AutoSize = true;
-            this.lblwait.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblwait.Location = new System.Drawing.Point(288, 217);
-            this.lblwait.Name = "lblwait";
-            this.lblwait.Size = new System.Drawing.Size(107, 25);
-            this.lblwait.TabIndex = 15;
-            this.lblwait.Text = "Loading...";
-            this.lblwait.Visible = false;
-            // 
             // num
             // 
-            this.num.Location = new System.Drawing.Point(416, 45);
+            this.num.Location = new System.Drawing.Point(155, 98);
             this.num.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -145,10 +149,10 @@
             0,
             0});
             this.num.Name = "num";
-            this.num.Size = new System.Drawing.Size(120, 20);
+            this.num.Size = new System.Drawing.Size(105, 20);
             this.num.TabIndex = 16;
             this.num.Value = new decimal(new int[] {
-            100,
+            10,
             0,
             0,
             0});
@@ -156,7 +160,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(413, 29);
+            this.label3.Location = new System.Drawing.Point(156, 78);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 17;
@@ -164,71 +168,218 @@
             // 
             // startreg
             // 
-            this.startreg.Location = new System.Drawing.Point(259, 45);
+            this.startreg.Location = new System.Drawing.Point(44, 98);
             this.startreg.Maximum = new decimal(new int[] {
             10000,
             0,
             0,
             0});
             this.startreg.Name = "startreg";
-            this.startreg.Size = new System.Drawing.Size(120, 20);
+            this.startreg.Size = new System.Drawing.Size(105, 20);
             this.startreg.TabIndex = 18;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(256, 29);
+            this.label4.Location = new System.Drawing.Point(41, 82);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 13);
             this.label4.TabIndex = 19;
             this.label4.Text = "Starting Register";
             // 
-            // overflow
-            // 
-            this.overflow.AutoSize = true;
-            this.overflow.Location = new System.Drawing.Point(674, 135);
-            this.overflow.Name = "overflow";
-            this.overflow.Size = new System.Drawing.Size(68, 17);
-            this.overflow.TabIndex = 20;
-            this.overflow.TabStop = true;
-            this.overflow.Text = "Extremes";
-            this.overflow.UseVisualStyleBackColor = true;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(686, 87);
+            this.label5.Location = new System.Drawing.Point(276, 29);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.Size = new System.Drawing.Size(80, 13);
             this.label5.TabIndex = 22;
-            this.label5.Text = "Ignore";
+            this.label5.Text = "Show Extremes";
             // 
-            // radioButton1
+            // slaveid
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(674, 112);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(62, 17);
-            this.radioButton1.TabIndex = 23;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Nothing";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.slaveid.Location = new System.Drawing.Point(220, 45);
+            this.slaveid.Name = "slaveid";
+            this.slaveid.Size = new System.Drawing.Size(41, 20);
+            this.slaveid.TabIndex = 27;
+            this.slaveid.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(217, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "ID";
+            // 
+            // port2
+            // 
+            this.port2.Location = new System.Drawing.Point(624, 45);
+            this.port2.Name = "port2";
+            this.port2.Size = new System.Drawing.Size(41, 20);
+            this.port2.TabIndex = 31;
+            this.port2.Text = "502";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(621, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Port";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(492, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "IP Address";
+            // 
+            // ipaddr2
+            // 
+            this.ipaddr2.Location = new System.Drawing.Point(495, 45);
+            this.ipaddr2.Name = "ipaddr2";
+            this.ipaddr2.Size = new System.Drawing.Size(123, 20);
+            this.ipaddr2.TabIndex = 28;
+            this.ipaddr2.Text = "127.0.0.1";
+            // 
+            // id1
+            // 
+            this.id1.Location = new System.Drawing.Point(521, 97);
+            this.id1.Name = "id1";
+            this.id1.Size = new System.Drawing.Size(41, 20);
+            this.id1.TabIndex = 33;
+            this.id1.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(545, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 13);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "ID Between";
+            // 
+            // id2
+            // 
+            this.id2.Location = new System.Drawing.Point(587, 97);
+            this.id2.Name = "id2";
+            this.id2.Size = new System.Drawing.Size(41, 20);
+            this.id2.TabIndex = 34;
+            this.id2.Text = "10";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(569, 101);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 13);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "->";
+            // 
+            // richedit2
+            // 
+            this.richedit2.Location = new System.Drawing.Point(437, 135);
+            this.richedit2.Name = "richedit2";
+            this.richedit2.Size = new System.Drawing.Size(288, 234);
+            this.richedit2.TabIndex = 36;
+            this.richedit2.Text = "";
+            // 
+            // lblwait
+            // 
+            this.lblwait.AutoSize = true;
+            this.lblwait.BackColor = System.Drawing.Color.Transparent;
+            this.lblwait.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwait.Location = new System.Drawing.Point(339, 224);
+            this.lblwait.Name = "lblwait";
+            this.lblwait.Size = new System.Drawing.Size(93, 20);
+            this.lblwait.TabIndex = 38;
+            this.lblwait.Text = "Searching...";
+            this.lblwait.Visible = false;
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(305, 256);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(163, 23);
+            this.pBar.TabIndex = 37;
+            this.pBar.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(437, 388);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 39;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // brkbox
+            // 
+            this.brkbox.AutoSize = true;
+            this.brkbox.Checked = true;
+            this.brkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.brkbox.Location = new System.Drawing.Point(681, 48);
+            this.brkbox.Name = "brkbox";
+            this.brkbox.Size = new System.Drawing.Size(44, 17);
+            this.brkbox.TabIndex = 42;
+            this.brkbox.Text = "Yes";
+            this.brkbox.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(678, 29);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 13);
+            this.label11.TabIndex = 43;
+            this.label11.Text = "Break once found";
+            // 
+            // showextrm
+            // 
+            this.showextrm.AutoSize = true;
+            this.showextrm.Checked = true;
+            this.showextrm.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showextrm.Location = new System.Drawing.Point(279, 48);
+            this.showextrm.Name = "showextrm";
+            this.showextrm.Size = new System.Drawing.Size(44, 17);
+            this.showextrm.TabIndex = 44;
+            this.showextrm.Text = "Yes";
+            this.showextrm.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.radioButton1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(770, 460);
+            this.Controls.Add(this.showextrm);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.brkbox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblwait);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.richedit2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.id2);
+            this.Controls.Add(this.id1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.port2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ipaddr2);
+            this.Controls.Add(this.slaveid);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.overflow);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.startreg);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.num);
-            this.Controls.Add(this.lblwait);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.save);
             this.Controls.Add(this.port);
@@ -237,7 +388,9 @@
             this.Controls.Add(this.ipadd);
             this.Controls.Add(this.richedit);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Modbus Scraper";
             ((System.ComponentModel.ISupportInitialize)(this.num)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.startreg)).EndInit();
@@ -256,14 +409,28 @@
         private System.Windows.Forms.RichTextBox richedit;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label lblwait;
         private System.Windows.Forms.NumericUpDown num;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown startreg;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RadioButton overflow;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox slaveid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox port2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox ipaddr2;
+        private System.Windows.Forms.TextBox id1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox id2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox richedit2;
+        private System.Windows.Forms.Label lblwait;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox brkbox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.CheckBox showextrm;
     }
 }
 
--- Modbus Scraper/Modbus Scraper/Form1.cs
+++ Modbus Scraper/Modbus Scraper/Form1.cs
@@ -15,6 +15,8 @@ namespace Modbus_Scraper
     public partial class Form1 : Form
     {
         String[] HoldingRegisters = new String[10000];
+        int arrsize = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -41,11 +43,20 @@ namespace Modbus_Scraper
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+           
+            pBar.Minimum = 0;
+            pBar.Maximum = 100;
+            int islave = Int32.Parse(slaveid.Text);
+            byte[] slave = BitConverter.GetBytes(islave);
             Array.Clear(HoldingRegisters, 0, HoldingRegisters.Length);
             int portnum = Int32.Parse(port.Text);
+
+            
             lblwait.Show();
+            pBar.Show();
+
             ModbusClient modbusClient = new ModbusClient(ipadd.Text, portnum);         //Ip-Address and Port of Modbus-TCP-Server
-            //modbusClient.UnitIdentifier = 77;
+            modbusClient.UnitIdentifier = slave[0];
             try
             {
                 modbusClient.Connect();                                                //Connect to Server
@@ -54,39 +65,41 @@ namespace Modbus_Scraper
             {
                 MessageBox.Show("No Modbus Server Detected");
             }
-
+            
+            
 
             int[] readHoldingRegisters = new int[10000];
-
             int startingreg = Decimal.ToInt32(startreg.Value);
             int pos = 0;
             int index1 = startingreg;
-
+            int amount = Decimal.ToInt32(num.Value);
+            arrsize = 0;
             for (int x = 0; x + startingreg < startingreg + num.Value; x++)
             {
                 try
                 {
                     readHoldingRegisters = modbusClient.ReadHoldingRegisters(startingreg + x, 1);
+                    HoldingRegisters[pos] = index1 + "   :  " + readHoldingRegisters[0].ToString();
+                    arrsize++;
 
-                    if (overflow.Checked)
-                    {
-                        if (readHoldingRegisters[0] != 0 && readHoldingRegisters[0] != -32768)
-                        {
-
-                            HoldingRegisters[pos] = index1 + "   :  " + readHoldingRegisters[0].ToString();
-                        }
-                    }
-                    else
-                    {
-
-                        HoldingRegisters[pos] = index1 + "   :  " + readHoldingRegisters[0].ToString();
-                    }
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("Node ID Doesnt Exist");
+                    break;
                 }
                 catch { }
                 index1++;
                 pos++;
+
+                int OldRange = (amount - 0);
+                int NewRange = (100 - 0);
+                int NewValue = (((x - 0) * NewRange) / OldRange) + 0;
+                pBar.Value = NewValue;
             }
             modbusClient.Disconnect();                                                //Disconnect from Server
+            pBar.Hide();
+            pBar.Value = 0;
             lblwait.Hide();
         }
 
@@ -118,19 +131,91 @@ namespace Modbus_Scraper
         private void button2_Click(object sender, EventArgs e)
         {
             richedit.Clear();
-            for (int i = 0; i < num.Value; i++)
+            for (int i = 0; i < arrsize; i++)
             {
-                if (!String.IsNullOrEmpty(HoldingRegisters[i]) || !String.IsNullOrWhiteSpace(HoldingRegisters[i]))
+
+                if (!showextrm.Checked)
                 {
-                    richedit.AppendText(HoldingRegisters[i]+ "\r\n");
+                    if(HoldingRegisters[i].Contains(":  0") || HoldingRegisters[i].Contains(":  -32768"))
+                    {
+                        //richedit.AppendText(HoldingRegisters[i] + "\r\n");
+                    }
+                    else
+                    {
+                        richedit.AppendText(HoldingRegisters[i] + "\n");
+                    }
+                }
+                else
+                {
+                    richedit.AppendText(HoldingRegisters[i] + "\n");
                     //richedit.ScrollToCaret();
                 }
             }
         }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
+            richedit2.Clear();
+            int portnum = Int32.Parse(port2.Text);
+            int num1 = Int32.Parse(id1.Text);
+            int num2 = int.Parse(id2.Text);
+            int[] ids = new int[10000];
+            int index = 0;
+            
+            ModbusClient modbusClient = new ModbusClient(ipaddr2.Text, portnum);         //Ip-Address and Port of Modbus-TCP-Server
+                                                                                         //for(int i = num1; i < num2; i++)
 
+            lblwait.Show();
+
+            modbusClient.Connect();                                                    //Connect to Server
+
+            pBar.Show();
+
+            for (int i = num1; i < num2; i++)
+            {
+                pBar.Minimum = 0;
+                pBar.Maximum = 100;
+
+                try
+                {
+                    byte[] slave = BitConverter.GetBytes(i);
+                    modbusClient.UnitIdentifier = slave[0];
+                    int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);    //Read 10 Holding Registers from Server, starting with Address 1
+                    ids[index] = i;
+                    richedit2.AppendText("Node ID : "+ids[index] + "\r\n");
+                    index++;
+                    if (brkbox.Checked)
+                    {
+                        break;
+                    }
+                }
+                catch (System.IO.IOException)
+                {
+                    //Node ID doesnt exist 
+                }
+                catch
+                {
+                    //Node ID exists but Registers could not be found
+                    ids[index] = i;
+                    richedit2.AppendText(ids[index] + "\r\n");
+                    index++;
+                    if (brkbox.Checked)
+                    {
+                        break;
+                    }
+
+                }
+                int OldRange = (num2 - num1);
+                int NewRange = (100 - 0);
+                int NewValue = (((i - 0) * NewRange) / OldRange) + 0;
+                pBar.Value = NewValue;
+
+
+            }
+            pBar.Value = 0;
+            pBar.Hide();
+            lblwait.Hide();
+
+            modbusClient.Disconnect();                                                //Disconnect from Server
         }
     }
 }
