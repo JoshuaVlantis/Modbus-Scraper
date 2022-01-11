14041ad6e31e83af9150c452001c4afa38af7901
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2022-01-11 16:37:13 +0200

    Added filters

--- Modbus Scraper/Modbus Scraper/Form1.Designer.cs
+++ Modbus Scraper/Modbus Scraper/Form1.Designer.cs
@@ -35,11 +35,22 @@
             this.ipadd = new System.Windows.Forms.TextBox();
             this.richedit = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblwait = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startreg = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.overflow = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startreg)).BeginInit();
             this.SuspendLayout();
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(142, 388);
+            this.save.Location = new System.Drawing.Point(239, 388);
             this.save.Name = "save";
             this.save.Size = new System.Drawing.Size(75, 23);
             this.save.TabIndex = 13;
@@ -99,11 +110,126 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Display";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblwait
+            // 
+            this.lblwait.AutoSize = true;
+            this.lblwait.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwait.Location = new System.Drawing.Point(288, 217);
+            this.lblwait.Name = "lblwait";
+            this.lblwait.Size = new System.Drawing.Size(107, 25);
+            this.lblwait.TabIndex = 15;
+            this.lblwait.Text = "Loading...";
+            this.lblwait.Visible = false;
+            // 
+            // num
+            // 
+            this.num.Location = new System.Drawing.Point(416, 45);
+            this.num.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.num.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(120, 20);
+            this.num.TabIndex = 16;
+            this.num.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(413, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Amount";
+            // 
+            // startreg
+            // 
+            this.startreg.Location = new System.Drawing.Point(259, 45);
+            this.startreg.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.startreg.Name = "startreg";
+            this.startreg.Size = new System.Drawing.Size(120, 20);
+            this.startreg.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(256, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Starting Register";
+            // 
+            // overflow
+            // 
+            this.overflow.AutoSize = true;
+            this.overflow.Location = new System.Drawing.Point(674, 135);
+            this.overflow.Name = "overflow";
+            this.overflow.Size = new System.Drawing.Size(68, 17);
+            this.overflow.TabIndex = 20;
+            this.overflow.TabStop = true;
+            this.overflow.Text = "Extremes";
+            this.overflow.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(686, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Ignore";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(674, 112);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 17);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nothing";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.overflow);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.startreg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.lblwait);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.save);
             this.Controls.Add(this.port);
             this.Controls.Add(this.label2);
@@ -112,7 +238,9 @@
             this.Controls.Add(this.richedit);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Modbus Scraper";
+            ((System.ComponentModel.ISupportInitialize)(this.num)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startreg)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +255,15 @@
         private System.Windows.Forms.TextBox ipadd;
         private System.Windows.Forms.RichTextBox richedit;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblwait;
+        private System.Windows.Forms.NumericUpDown num;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown startreg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton overflow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- Modbus Scraper/Modbus Scraper/Form1.cs
+++ Modbus Scraper/Modbus Scraper/Form1.cs
@@ -14,6 +14,7 @@ namespace Modbus_Scraper
 {
     public partial class Form1 : Form
     {
+        String[] HoldingRegisters = new String[10000];
         public Form1()
         {
             InitializeComponent();
@@ -40,25 +41,53 @@ namespace Modbus_Scraper
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            Array.Clear(HoldingRegisters, 0, HoldingRegisters.Length);
             int portnum = Int32.Parse(port.Text);
+            lblwait.Show();
             ModbusClient modbusClient = new ModbusClient(ipadd.Text, portnum);         //Ip-Address and Port of Modbus-TCP-Server
-            modbusClient.Connect();                                                    //Connect to Server
+            //modbusClient.UnitIdentifier = 77;
+            try
+            {
+                modbusClient.Connect();                                                //Connect to Server
+            }
+            catch
+            {
+                MessageBox.Show("No Modbus Server Detected");
+            }
+
 
             int[] readHoldingRegisters = new int[10000];
-            int index1 = 0;
 
-            for (int x = 0; x < 100; x++)
+            int startingreg = Decimal.ToInt32(startreg.Value);
+            int pos = 0;
+            int index1 = startingreg;
+
+            for (int x = 0; x + startingreg < startingreg + num.Value; x++)
             {
-                readHoldingRegisters = modbusClient.ReadHoldingRegisters(x * 100, 100);
-
-                for (int i = 0; i < 100; i++)
+                try
                 {
-                    richedit.AppendText("\r\n" + +(index1) + "    :    " + readHoldingRegisters[i].ToString());
-                    richedit.ScrollToCaret();
-                    index1++;
+                    readHoldingRegisters = modbusClient.ReadHoldingRegisters(startingreg + x, 1);
+
+                    if (overflow.Checked)
+                    {
+                        if (readHoldingRegisters[0] != 0 && readHoldingRegisters[0] != -32768)
+                        {
+
+                            HoldingRegisters[pos] = index1 + "   :  " + readHoldingRegisters[0].ToString();
+                        }
+                    }
+                    else
+                    {
+
+                        HoldingRegisters[pos] = index1 + "   :  " + readHoldingRegisters[0].ToString();
+                    }
                 }
+                catch { }
+                index1++;
+                pos++;
             }
             modbusClient.Disconnect();                                                //Disconnect from Server
+            lblwait.Hide();
         }
 
         private void save_Click_1(object sender, EventArgs e)
@@ -84,6 +113,24 @@ namespace Modbus_Scraper
                 catch
                 { }
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richedit.Clear();
+            for (int i = 0; i < num.Value; i++)
+            {
+                if (!String.IsNullOrEmpty(HoldingRegisters[i]) || !String.IsNullOrWhiteSpace(HoldingRegisters[i]))
+                {
+                    richedit.AppendText(HoldingRegisters[i]+ "\r\n");
+                    //richedit.ScrollToCaret();
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
