ee57f48190f473e8af688b3577bd6e1c4d5333a8
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2022-10-03 16:32:17 +0200

    Added Stefans Trending Page
    Basic Trending setup

--- Home.cs
+++ Home.cs
@@ -889,7 +889,8 @@ namespace EasyBus_Modbus_Scanner
 
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            Form trnd = new Trending();
+            trnd.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
--- Trending.Designer.cs
+++ Trending.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(800, 450);
+            this.cartesianChart1.TabIndex = 2;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Trending
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Trending";
             this.Text = "Trending";
+            this.Load += new System.EventHandler(this.Trending_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
     }
 }
